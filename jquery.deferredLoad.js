1c053f8f3f5da6d297682ce2c7786f7627a570a6
Steve Bering <devnull@localhost>
2011-03-26 06:39:07 -0700

    moved to using animage

--- jquery.deferredLoad.js
+++ jquery.deferredLoad.js
@@ -13,12 +13,17 @@
 			if ( options ) {
 				$.extend( settings, options );
 			}
-		
-			this
-				.html('<img src="' + settings.loadingImage + '" />')
-				.queue()
-				.delay(settings.waitingTime)
-				.load(url, callback);
+			
+			$('<img src="' + settings.loadingImage + '" />')
+				.insertAfter($(this))
+				.fadeIn('slow')
+				.animate({opacity: 1.0}, settings.waitingTime, function() {
+					$(this).load(url, callback)
+						.fadeIn('slow');
+				})
+				.fadeOut('slow', 
+					$(this).remove();
+				)
 		});	
 	}
 
